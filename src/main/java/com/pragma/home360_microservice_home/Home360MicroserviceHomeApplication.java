acfe9f39e3e5d47bb1846e6bb1dc9dc173e63159
Brayan Rodallega <thebrayan1a1@outlook.com>
2025-03-19 05:00:49 +0000

    initial project

---
+++ src/main/java/com/pragma/home360_microservice_home/Home360MicroserviceHomeApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.home360_microservice_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home360MicroserviceHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Home360MicroserviceHomeApplication.class, args);
+	}
+
+}

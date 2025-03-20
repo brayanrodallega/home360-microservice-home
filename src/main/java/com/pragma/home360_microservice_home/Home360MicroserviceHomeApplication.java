62ba96586c5ba626851b17fdfa0d73d22fb0c10a
Brayan Rodallega <thebrayan1a1@outlook.com>
2025-03-20 23:01:25 +0000

    Project configuration

--- src/main/java/com/pragma/home360_microservice_home/Home360MicroserviceHomeApplication.java
+++ src/main/java/com/pragma/home360_microservice_home/Home360MicroserviceHomeApplication.java
@@ -1,13 +1,20 @@
 package com.pragma.home360_microservice_home;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Home360MicroserviceHomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Home360MicroserviceHomeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Cargar las variables de entorno desde el archivo .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Establecer las variables de entorno en el entorno de la JVM
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(Home360MicroserviceHomeApplication.class, args);
+    }
 
 }
